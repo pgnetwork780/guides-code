b6e9609f0a3396ec5b893fbaf735fdfe2a011720
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-04 13:56:32 +0530

    update js/achievements-leaderboard.js

--- js/achievements-leaderboard.js
+++ js/achievements-leaderboard.js
@@ -4,9 +4,9 @@ const achievementsData = [
     { rank: 3, name: "MythicalPanda30", achievements: "60" },
     { rank: 4, name: "Dharmaveer31", achievements: "57" },
     { rank: 5, name: "Perry3338", achievements: "51" },
-    { rank: 6, name: "Kautilya3072", achievements: "46" },
+    { rank: 6, name: "TGJASSIYT", achievements: "49" },
+    { rank: 7, name: "Kautilya3072", achievements: "46" },
     { rank: 8, name: "Jeremy2075", achievements: "33" },
-    { rank: 7, name: "SAAD_KING_07726", achievements: "23" },
-    { rank: 9, name: "Unknown", achievements: "0" },
+    { rank: 9, name: "SAAD_KING_07726", achievements: "23" },
     { rank: 10, name: "Unknown", achievements: "0" }
 ];
