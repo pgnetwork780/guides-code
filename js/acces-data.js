5127dd5ba4cb879492aac3d7c4cd88f1fb107a28
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-08 00:01:20 +0530

    update js/acces-data.js

---
+++ js/acces-data.js
@@ -0,0 +1,17 @@
+const accessories = [
+      {
+        id: "campfire_initiate_badge",
+        name: "test",
+        icon: "img/Cobblestone.png"
+      },
+      {
+        id: "fire_talisman",
+        name: "Fire Talisman",
+        icon: "img/Cobblestone.png"
+      },
+      {
+        id: "coin_ring",
+        name: "Coin Ring",
+        icon: "img/Cobblestone.png"
+      }
+    ];
