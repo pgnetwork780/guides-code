671c4ea6ca64b21a8368216340026316acc19698
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-09 00:37:31 +0530

    update js/cakes-leaderboard.js

--- js/cakes-leaderboard.js
+++ js/cakes-leaderboard.js
@@ -1,7 +1,7 @@
 const cakeData = [
   { rank: 1, name: "MythicalPanda30", cakes: "172" },
-  { rank: 2, name: "Ghosthuner4233", cakes: "26" },
-  { rank: 3, name: "Unknown", cakes: "N/A" },
+  { rank: 2, name: "TGJASSIYT", cakes: "74" },
+  { rank: 3, name: "Ghosthuner4233", cakes: "26" },
   { rank: 4, name: "Unknown", cakes: "N/A" },
   { rank: 5, name: "Unknown", cakes: "N/A" },
   { rank: 6, name: "Unknown", cakes: "N/A" },
