952e24428d9efb722d735ef7ceb282b4513ddce1
Anshuman Mishra <gamerzp780@gmail.com>
2025-11-08 20:52:55 +0530

    Update achievements-leaderboard.js

--- js/achievements-leaderboard.js
+++ js/achievements-leaderboard.js
@@ -6,7 +6,7 @@ const achievementsData = [
     { rank: 5, name: "Perry3338", achievements: "51" },
     { rank: 6, name: "TGJASSIYT", achievements: "49" },
     { rank: 7, name: "Kautilya3072", achievements: "46" },
-    { rank: 8, name: "Jeremy2075", achievements: "33" },
-    { rank: 9, name: "SAAD_KING_07726", achievements: "23" },
-    { rank: 10, name: "Unknown", achievements: "0" }
+    { rank: 8, name: "Darkshadow52195", achievements: "41" },
+    { rank: 9, name: "Jeremy2075", achievements: "33" },
+    { rank: 10, name: "SAAD_KING_07726", achievements: "23" }
 ];
