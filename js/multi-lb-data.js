296f7a1919d0e101d4a9867e8cc0914b09fedc12
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-10 21:03:58 +0530

    update js/multi-lb-data.js

--- js/multi-lb-data.js
+++ js/multi-lb-data.js
@@ -17,14 +17,14 @@ const achievementsData = [
 const skillData = [
   { rank: 1, name: "SphinxWraith/David_R06", level: "51.7" },
   { rank: 2, name: "Moerderfisch", level: "50.7" },
-  { rank: 3, name: "AndyGamer024/GRAVITY8303", level: "50" },
-  { rank: 4, name: "Itzomkar1232891", level: "47.2" },
-  { rank: 5, name: "DRONZAR_OP", level: "46.2" },
-  { rank: 6, name: "Kg_lcky_01", level: "46" },
-  { rank: 7, name: "Rehan2038", level: "42.3" },
-  { rank: 8, name: "Perry3338", level: "39.5" },
-  { rank: 9, name: "Kautilya3072", level: "36.2" },
-  { rank: 10, name: "Jatinsaini900", level: "35.7" }
+  { rank: 3, name: "GRAVITY8303", level: "50.2" },
+  { rank: 4, name: "AndyGamer024", level: "50" },
+  { rank: 5, name: "Itzomkar1232891", level: "47.2" },
+  { rank: 6, name: "DRONZAR_OP", level: "46.2" },
+  { rank: 7, name: "Kg_lcky_01", level: "46" },
+  { rank: 8, name: "Rehan2038", level: "42.3" },
+  { rank: 9, name: "Perry3338", level: "39.5" },
+  { rank: 10, name: "Kautilya3072", level: "36.2" }
 ];
 // Cakes
 const cakeData = [
