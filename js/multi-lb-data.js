79e6df11c53a86d6ebdb4f077328a7db23b7985d
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-12 22:25:16 +0530

    update js/multi-lb-data.js

--- js/multi-lb-data.js
+++ js/multi-lb-data.js
@@ -31,7 +31,7 @@ const cakeData = [
   { rank: 1, name: "MythicalPanda30", cakes: "172" },
   { rank: 2, name: "TGJASSIYT", cakes: "74" },
   { rank: 3, name: "Ghosthuner4233", cakes: "26" },
-  { rank: 4, name: "Unknown", cakes: "N/A" },
+  { rank: 4, name: "C69P2W", cakes: "5" },
   { rank: 5, name: "Unknown", cakes: "N/A" },
   { rank: 6, name: "Unknown", cakes: "N/A" },
   { rank: 7, name: "Unknown", cakes: "N/A" },
