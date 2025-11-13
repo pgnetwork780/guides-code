e0dedfec6417d471aa3a23f8fa74b30e6c3e33f5
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-13 16:33:57 +0530

    update js/multi-lb-data.js

--- js/multi-lb-data.js
+++ js/multi-lb-data.js
@@ -113,3 +113,16 @@ const collectionsData = [
     ]
   }
 ];
+// Damage
+const damageData = [
+  { rank: 1, name: "Unknown", damage: "N/A" },
+  { rank: 2, name: "Unknown", damage: "N/A" },
+  { rank: 3, name: "Unknown", damage: "N/A" },
+  { rank: 4, name: "Unknown", damage: "N/A" },
+  { rank: 5, name: "Unknown", damage: "N/A" },
+  { rank: 6, name: "Unknown", damage: "N/A" },
+  { rank: 7, name: "Unknown", damage: "N/A" },
+  { rank: 8, name: "Unknown", damage: "N/A" },
+  { rank: 9, name: "Unknown", damage: "N/A" },
+  { rank: 10, name: "Unknown", damage: "N/A" }
+];
